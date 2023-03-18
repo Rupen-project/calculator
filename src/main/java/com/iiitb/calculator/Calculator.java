5e9d279300fd5e42e6c061b22837c4e1860c64d6
Rupen-project <rupenrakholiya559@gmail.com>
2023-03-18 10:25:39 +0530

    Test cases and log file done

--- src/main/java/com/iiitb/calculator/Calculator.java
+++ src/main/java/com/iiitb/calculator/Calculator.java
@@ -1,35 +1,159 @@
 package com.iiitb.calculator;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
+class CalcOperations{
 
+    private static final Logger logger = LogManager.getLogger(CalcOperations.class);
+    public double add(double a,double b){
+        double ans = a+b;
+            logger.info("[ADDITION of "+a+" AND "+b+"][RESULT : "+ ans+"]");
+        return ans;
+    }
+
+    public double subtract(double a,double b){
+        double ans = a-b;
+            logger.info("[SUBTRACTION of "+a+" AND "+b+"][RESULT : "+ ans+"]");
+        return ans;
+    }
+
+    public double multiply(double a,double b){
+
+        double ans = a*b;
+            logger.info("[MULTIPLICATION of "+a+" AND "+b+"][RESULT : "+ ans+"]");
+        return ans;
+
+    }
+
+    public double divide(double a,double b){
+
+        double ans = a/b;
+            logger.info("[DIVISION of "+a+" AND "+b+"][RESULT : "+ ans+"]");
+        return ans;
+    }
+
+    public double squareRoot(double a){
+        double ans = Math.sqrt(a);
+            logger.info("[SQUARE ROOT of "+a+"][RESULT : "+ ans+"]");
+        return ans;
+    }
+
+    public double factorial(int n){
+
+        if(n<0)
+            return Double.NaN;
+
+        long ans = 1;
+        while(n > 0){
+            ans = ans*n;
+            n--;
+        }
+            logger.info("[FACTORIAL of "+n+"][RESULT : "+ ans+"]");
+        return ans;
+    }
+
+    public double naturalLog(double n){
+        double ans = Math.log(n);
+            logger.info("[NATURAL LOG of "+n+"][RESULT : "+ ans+"]");
+        return ans;
+    }
+
+    public double power(double a,double b){
+        double ans = Math.pow(a,b);
+            logger.info("[POWER of "+a+" AND "+b+"][RESULT : "+ ans+"]");
+        return ans;
+    }
+}
 public class Calculator {
 
 
-    public int add(int a, int b){
-        return a+b;
-    }
+
     public void run() {
-        int num1, num2, num3;
-        boolean flag = true;
-        char choice;
-        Scanner scan = new Scanner(System.in);
-        while(flag){
-            try{
-                System.out.println("Enter your two integers(space separated)");
-                num1 = scan.nextInt();
-                num2 = scan.nextInt();
-                num3 = add(num1,num2);
-                System.out.println(num3);
-                System.out.println("You wanna continue (Y/N) ? : ");
-                choice = scan.next().charAt(0);
-                if(choice!='Y'){
-                    flag = false;
+        Scanner scanner = new Scanner(System.in);
+        CalcOperations calcOperations = new CalcOperations();
+
+        int op;
+        double num1=0,num2=0;
+        int ni=0;
+
+        scanner.next().charAt(0);
+
+
+        System.out.println("*********************** Calculator ***********************\n");
+
+
+        while(true){
+            try {
+
+
+                System.out.print("****** Select Operations ******\n\n0 -> Exit\n1 -> Addition\n2 -> Subtraction\n3 -> Multiplication\n4 -> Division\n5 -> Square root\n6 -> Factorial\n7 -> Natural log\n8 -> Power\n\nSelect one of the operation no. : ");
+
+                op = scanner.nextInt();
+
+                if (op == 0) {
+                    System.out.println("Bye Bye...\n");
+                    System.out.println("Thank you for your time...\n");
+                    System.out.println("RUPENKUMAR RAKHOLIYA ( MT2022093 ) \n");
+                    break;
                 }
-            }
-            catch(Exception e){
-                System.out.println(e);
-                return;
+
+                if(op == 5){
+                    System.out.print("Provide any non-negative real number : ");
+                    num1 = scanner.nextDouble();
+                }else if(op == 6){
+                    System.out.print("Provide any positive integer number in range (1-20) : ");
+                    ni = scanner.nextInt();
+
+                }else if(op == 7){
+                    System.out.print("Provide any non-negative real number : ");
+                    num1 = scanner.nextDouble();
+                }
+                else if(op>=1 && op<=8){
+                    System.out.print("Provide 1st operand : ");
+                    num1 = scanner.nextDouble();
+                    System.out.print("Provide 2nd operand : ");
+                    num2 = scanner.nextDouble();
+                }
+
+                switch (op) {
+
+                    case 1:
+                        System.out.println(num1 + " + " + num2 + " is " + calcOperations.add(num1, num2) + "\n\n");
+                        break;
+
+                    case 2:
+                        System.out.println(num1 + " - " + num2 + " is " + calcOperations.subtract(num1, num2) + "\n\n");
+                        break;
+
+                    case 3:
+                        System.out.println(num1 + " * " + num2 + " is " + calcOperations.multiply(num1, num2) + "\n\n");
+                        break;
+
+                    case 4:
+                        System.out.println(num1 + " / " + num2 + " is " + calcOperations.divide(num1, num2) + "\n\n");
+                        break;
+                    case 5:
+                        System.out.println("sqrt("+num1+")"+" is " + calcOperations.squareRoot(num1) + "\n\n");
+                        break;
+                    case 6:
+                        System.out.println(ni + "! is " +calcOperations.factorial(ni)+"\n\n");
+                        break;
+                    case 7:
+                        System.out.println("ln("+num1+") is "+ calcOperations.naturalLog(num1)+"\n\n");
+                        break;
+                    case 8:
+                        System.out.println("pow("+num1+","+num2+") is "+ calcOperations.power(num1,num2)+"\n\n");
+                        break;
+                    default:
+                        System.out.println("Please provide correct operation no. from above list only");
+                }
+            }catch(Exception e){
+
+                System.out.println("\n\n Something went wrong...please make sure you have entered correct inputs as asked\n\nE.g. \n-Don't enter -ve values for log operations\n-Only natural numbers are allowed for factorial ...etc\n\n");
+                scanner.nextLine();
             }
         }
     }
--- src/test/java/com/iiitb/calculator/CalculatorTest.java
+++ src/test/java/com/iiitb/calculator/CalculatorTest.java
@@ -5,11 +5,110 @@ import org.junit.Test;
 
 public class CalculatorTest{
 
-    Calculator calc = new Calculator();
+//    Calculator calc = new Calculator();
+    CalcOperations calcOperations = new CalcOperations();
+    private final double delta =0.000000000001;
+
     @Test
-    public void addTest(){
-        Assert.assertEquals(11, calc.add(7, 4));
-        Assert.assertEquals(10, calc.add(5, 5));
+    public void add() {
+        Assert.assertEquals(10,calcOperations.add(8,2),delta);
+        Assert.assertEquals(10,calcOperations.add(5,5),delta);
     }
+    @Test
+    public void addFalsePositive(){
+        Assert.assertNotEquals(12,calcOperations.add(2,5),delta);
+        Assert.assertNotEquals(0,calcOperations.add(4,4),delta);
+    }
+
+    @Test
+    public void subtract() {
+        Assert.assertEquals(0,calcOperations.subtract(4,4),delta);
+        Assert.assertEquals(0,calcOperations.subtract(-4,-4),delta);
+        Assert.assertEquals(-4,calcOperations.subtract(4,8),delta);
+    }
+    @Test
+    public void subtractFalsePositive(){
+        Assert.assertNotEquals(10,calcOperations.subtract(10,2),delta);
+        Assert.assertNotEquals(14,calcOperations.subtract(16,-2),delta);
+    }
+
+    @Test
+    public void multiply() {
+        Assert.assertEquals(0,calcOperations.multiply(0,1),delta);
+        Assert.assertEquals(10,calcOperations.multiply(5,2),delta);
+    }
+
+    @Test
+    public void multiplyFalsePositive(){
+        Assert.assertNotEquals(10,calcOperations.multiply(10,0),delta);
+        Assert.assertNotEquals(25,calcOperations.multiply(5,1),delta);
+    }
+
+    @Test
+    public void divide() {
+        Assert.assertEquals(2,calcOperations.divide(10,5),delta);
+        Assert.assertEquals(Double.POSITIVE_INFINITY,calcOperations.divide(1,0),delta);
+        Assert.assertEquals(Double.NEGATIVE_INFINITY,calcOperations.divide(-1,0),delta);
+    }
+
+    @Test
+    public void divideFalsePositive(){
+        Assert.assertNotEquals(3,calcOperations.divide(6,3),delta);
+        Assert.assertNotEquals(6,calcOperations.divide(6,0),delta);
+    }
+
+    @Test
+    public void squareRoot(){
+        Assert.assertEquals(4.0,calcOperations.squareRoot(16),delta);
+        Assert.assertEquals(Math.sqrt(1564.564),calcOperations.squareRoot(1564.564),delta);
+        Assert.assertEquals(Double.NaN,calcOperations.squareRoot(-2),delta);
+    }
+
+    @Test
+    public void squareRootFalsePositive(){
+        Assert.assertNotEquals(2,calcOperations.squareRoot(5),delta);
+        Assert.assertNotEquals(2,calcOperations.squareRoot(-4),delta);
+    }
+
+    @Test
+    public void fatorial(){
+        Assert.assertEquals(120,calcOperations.factorial(5),delta);
+        Assert.assertEquals(1,calcOperations.factorial(0),delta);
+        Assert.assertEquals(1,calcOperations.factorial(1),delta);
+        Assert.assertEquals(Double.NaN,calcOperations.factorial(-2),delta);
+    }
+
+    public void factorialFalsePositive(){
+        Assert.assertNotEquals(24,calcOperations.factorial(-4),delta);
+        Assert.assertNotEquals(0,calcOperations.factorial(0),delta);
+    }
+
+    @Test
+    public void naturalLog(){
+        Assert.assertEquals(1,calcOperations.naturalLog(Math.exp(1)),delta);
+        Assert.assertEquals(Math.log(23.56),calcOperations.naturalLog(23.56),delta);
+        Assert.assertEquals(Math.log(0),calcOperations.naturalLog(0),delta);
+        Assert.assertEquals(Double.NEGATIVE_INFINITY,calcOperations.naturalLog(0),delta);
+    }
+
+    @Test
+    public void naturalLogFalsePositive(){
+        Assert.assertNotEquals(0,calcOperations.naturalLog(0),delta);
+    }
+
+    @Test
+    public void power(){
+        Assert.assertEquals(16,calcOperations.power(4,2),delta);
+        Assert.assertEquals(1,calcOperations.power(1,0),delta);
+        Assert.assertEquals(Double.NaN,calcOperations.power(-2,-0.99),delta);
+        Assert.assertEquals(-0.125,calcOperations.power(-2,-3),delta);
+    }
+
+    @Test
+    public void powerFalsePositive(){
+        Assert.assertNotEquals(-2,calcOperations.power(-4,-0.5),delta);
+    }
+
+
 
 }
